351b594444c0ced2f8356ead65a2a3f76970869e
Kazakov Maksim <kazakovmaksimi@gmail.com>
2022-02-22 00:41:03 +0400

    refactor: add developers tasks and clearing LS on sign out

--- src/data/developers.ts
+++ src/data/developers.ts
@@ -2,14 +2,14 @@ export const developers = [
   {
     photo: 'Max',
     name: 'Максим Казаков',
-    description: ['Роутер', 'Меню'],
+    description: ['Роутер', 'Меню', '«Спринт»', 'Авторизация'],
     githubHref: 'https://github.com/KazakovMaksim?tab=repositories',
     github: 'KazakovMaksim',
   },
   {
     photo: 'Lenar',
     name: 'Ленар Фасхиев',
-    description: ['Стартовая страница'],
+    description: ['Стартовая страница', 'Статистика', '"Аудио вызов"', 'Учебник и словарь'],
     githubHref: 'https://github.com/LenarFF?tab=repositories',
     github: 'LenarFF',
   },
